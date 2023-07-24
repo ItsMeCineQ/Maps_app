40eb43541fdf1d9791a1968814c45964ff6f40e6
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-07-24 15:46:34 +0200

    Geolocation API

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.input_select-type');
+const inputDistance = document.querySelector('.input-distance');
+const inputTime = document.querySelector('.input-time');
+const inputCadence = document.querySelector('.input-cadence');
+const inputDuration = document.querySelector('.input-duration');
+const inputElevation = document.querySelector('.input-elevation');
+
+// Geolocation API
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        }, 
+        function(){
+            alert('Could not get your position');
+        });
+}
