4f4e63e51873ddf28ad786e4dd1db14d2ad49799
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-07-26 15:01:03 +0200

    Workout, running and cycling class

--- main.js
+++ main.js
@@ -10,6 +10,48 @@ const inputCadence = document.querySelector('.input_cadence');
 const inputDuration = document.querySelector('.input_duration');
 const inputElevation = document.querySelector('.input_elevation');
 
+class Workout{
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+    constructor(coords, distance, duration){
+        this.coords = coords; // [lat, lng]
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
+
+class Running extends Workout{
+    constructor(coords, distance, duration, cadence){
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace(){
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout{
+    constructor(coords, distance, duration, elevation){
+        super(coords, distance, duration);
+        this.elevation = elevation;
+        this.calcSpeed();
+    }
+
+    calcSpeed(){
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+    }
+}
+
+const run1 = new Running([50, 39], 5.2, 24, 178);
+const cycling1 = new Cycling([50, 39], 27, 95, 523);
+
+console.log(run1, cycling1);
+
+// APPLICATION ARCHITECTURE
 class App {
     #map;
     #mapEvent;
